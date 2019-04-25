73399f92e64c8d1e538f9bb6f11ade122141f811
Francesco Aiello <aiellof@oregonstate.edu>
2019-04-24 19:12:33 -0700

    dropdown fix

--- assignment3/js/main.js
+++ assignment3/js/main.js
@@ -1,4 +1,5 @@
-var images = [], x = 0;
+var images = [],
+    x = 0;
 images[0] = "images/image1.jpg";
 images[1] = "images/image2.jpg";
 images[2] = "images/image3.jpg";
@@ -25,42 +26,42 @@ function nextImage() {
 
 //tried to do this in a loop, but strings tripped me up
 var clearing = document.getElementById("clear");
-clearing.onclick = function(){
-	var thingy = document.getElementById("container1").style;
-	thingy.display = "none";
-	var thingy = document.getElementById("container2").style;
-	thingy.display = "none";
-	var thingy = document.getElementById("container3").style;
-	thingy.display = "none";
-	var thingy = document.getElementById("container4").style;
-	thingy.display = "none";
-	var thingy = document.getElementById("container5").style;
-	thingy.display = "none";
-	var thingy = document.getElementById("container6").style;
-	thingy.display = "none";
-	var thingy = document.getElementById("container7").style;
-	thingy.display = "none";
-	var thingy = document.getElementById("container8").style;
-	thingy.display = "none";
+clearing.onclick = function() {
+    var thingy = document.getElementById("container1").style;
+    thingy.display = "none";
+    var thingy = document.getElementById("container2").style;
+    thingy.display = "none";
+    var thingy = document.getElementById("container3").style;
+    thingy.display = "none";
+    var thingy = document.getElementById("container4").style;
+    thingy.display = "none";
+    var thingy = document.getElementById("container5").style;
+    thingy.display = "none";
+    var thingy = document.getElementById("container6").style;
+    thingy.display = "none";
+    var thingy = document.getElementById("container7").style;
+    thingy.display = "none";
+    var thingy = document.getElementById("container8").style;
+    thingy.display = "none";
 }
 var resetting = document.getElementById("reset");
-resetting.onclick = function(){
-	var thingy = document.getElementById("container1").style;
-	thingy.display = "block";
-	var thingy = document.getElementById("container2").style;
-	thingy.display = "block";
-	var thingy = document.getElementById("container3").style;
-	thingy.display = "block";
-	var thingy = document.getElementById("container4").style;
-	thingy.display = "block";
-	var thingy = document.getElementById("container5").style;
-	thingy.display = "block";
-	var thingy = document.getElementById("container6").style;
-	thingy.display = "block";
-	var thingy = document.getElementById("container7").style;
-	thingy.display = "block";
-	var thingy = document.getElementById("container8").style;
-	thingy.display = "block";
+resetting.onclick = function() {
+    var thingy = document.getElementById("container1").style;
+    thingy.display = "block";
+    var thingy = document.getElementById("container2").style;
+    thingy.display = "block";
+    var thingy = document.getElementById("container3").style;
+    thingy.display = "block";
+    var thingy = document.getElementById("container4").style;
+    thingy.display = "block";
+    var thingy = document.getElementById("container5").style;
+    thingy.display = "block";
+    var thingy = document.getElementById("container6").style;
+    thingy.display = "block";
+    var thingy = document.getElementById("container7").style;
+    thingy.display = "block";
+    var thingy = document.getElementById("container8").style;
+    thingy.display = "block";
 }
 
 var ele1 = document.getElementById("box1");
@@ -71,44 +72,43 @@ var ele5 = document.getElementById("box5");
 var ele6 = document.getElementById("box6");
 var ele7 = document.getElementById("box7");
 var ele8 = document.getElementById("box8");
-ele1.onclick = function(){
-	var thingy = document.getElementById("container1").style;
-	thingy.display = "none";
+ele1.onclick = function() {
+    var thingy = document.getElementById("container1").style;
+    thingy.display = "none";
 
 }
-ele2.onclick = function(){
-	var thingy = document.getElementById("container2").style;
-	thingy.display = "none";
+ele2.onclick = function() {
+    var thingy = document.getElementById("container2").style;
+    thingy.display = "none";
 
 }
-ele3.onclick = function(){
-	var thingy = document.getElementById("container3").style;
-	thingy.display = "none";
+ele3.onclick = function() {
+    var thingy = document.getElementById("container3").style;
+    thingy.display = "none";
 
 }
-ele4.onclick = function(){
-	var thingy = document.getElementById("container4").style;
-	thingy.display = "none";
+ele4.onclick = function() {
+    var thingy = document.getElementById("container4").style;
+    thingy.display = "none";
 
 }
-ele5.onclick = function(){
-	var thingy = document.getElementById("container5").style;
-	thingy.display = "none";
+ele5.onclick = function() {
+    var thingy = document.getElementById("container5").style;
+    thingy.display = "none";
 
 }
-ele6.onclick = function(){
-	var thingy = document.getElementById("container6").style;
-	thingy.display = "none";
+ele6.onclick = function() {
+    var thingy = document.getElementById("container6").style;
+    thingy.display = "none";
 
 }
-ele7.onclick = function(){
-	var thingy = document.getElementById("container7").style;
-	thingy.display = "none";
+ele7.onclick = function() {
+    var thingy = document.getElementById("container7").style;
+    thingy.display = "none";
 
 }
-ele8.onclick = function(){
-	var thingy = document.getElementById("container8").style;
-	thingy.display = "none";
+ele8.onclick = function() {
+    var thingy = document.getElementById("container8").style;
+    thingy.display = "none";
 
 }
-
